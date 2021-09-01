8d18b625e03b66349a60062f25bd4b9a8a6bdcfe
hapt <hak2cqn@gmail.com>
2021-09-01 10:29:44 +0700

    collection menu

--- src/features/Collection/components/LeftSidebar.js
+++ src/features/Collection/components/LeftSidebar.js
@@ -1,39 +1,61 @@
 import { Collapse } from "bootstrap";
 import { useState, useEffect, memo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Link, useParams } from "react-router-dom";
 
 import * as Unicons from "@iconscout/react-unicons";
 
 function LeftSidebar({ sortCondition, setSortCondition }) {
   const { collections } = useSelector((state) => state.collections);
+  const [active, setActive] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+    setActive(params.collectionId ? params.collectionId : null);
+  }, [params]);
+
   let [toggle, setToggle] = useState(false);
 
   useEffect(() => {
-    let myCollapse = document.getElementById("left-sidebar-content");
+    let myCollapse = document.getElementById("left__sidebar--content");
     let bsCollapse = new Collapse(myCollapse, { toggle: false });
     toggle ? bsCollapse.show() : bsCollapse.hide();
   });
 
   return (
-    <aside className="col-lg-3 left-sidebar">
+    <aside className="col-lg-3 left__sidebar">
       <div className="d-lg-none">
         <a
-          className="d-block text-center mobile-refine"
+          className="d-block text-center left__sidebar--mobile-refine"
           onClick={() => setToggle(!toggle)}
         >
           <Unicons.UilBars size="24" color="#000000" /> Refine
         </a>
       </div>
-      <div id="left-sidebar-content">
-        {/*<div className="sidebar-module collection-second-image">
-          {typeof currentCollection.images != "undefined" && (
-            <img
-              src={process.env.REACT_APP_API_URL + currentCollection.images[1]}
-              alt=""
-            />
-          )}
-          </div>*/}
-        <div className="sidebar-module sort-product">
+      <div id="left__sidebar--content">
+        <div className="left__sidebar--module choose-collections">
+          <ul>
+            <li>
+              <Link
+                className={active === null ? "active" : ""}
+                to="/collections"
+              >
+                All Products
+              </Link>
+            </li>
+            {collections.map((item) => (
+              <li key={item.id}>
+                <Link
+                  className={active == item.id ? "active" : ""}
+                  to={"/collections/" + item.id + "/" + item.slug}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="left__sidebar--module sort-product">
           <select
             name="sort-select"
             id="sort-select"
@@ -48,9 +70,9 @@ function LeftSidebar({ sortCondition, setSortCondition }) {
             <option value="price-descending">Price, high to low</option>
           </select>
         </div>
-        <div className="sidebar-module sort-product">
-          <h4 className="module-title">Shop with confidence</h4>
-          <div className="module-content">
+        <div className="left__sidebar--module sort-product">
+          <h4 className="left__sidebar--module-title">Shop with confidence</h4>
+          <div className="left__sidebar--module-content">
             <p>
               <span style={{ textDecoration: "underline" }}>Free</span>
               Delivery and
